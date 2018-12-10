627ad9e13e1c624b079bca641453d7dc48e5d0cd
StormFalcon32 <benchen9708@gmail.com>
2018-12-09 22:03:06 -0500

    Started subset

--- Training/src/subset.java
+++ Training/src/subset.java
@@ -1,3 +1,4 @@
+
 /*
 ID: benchen1
 LANG: JAVA
@@ -13,16 +14,42 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class subset {
-	
+
 	static int numInts;
+	static int totalSum;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("subset.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("subset.out")));
 		StringTokenizer ln = new StringTokenizer(in.readLine());
 		numInts = Integer.parseInt(ln.nextToken());
-		
+		int[] ints = new int[numInts];
+		for (int i = 0; i < numInts; i++) {
+			ints[i] = i + 1;
+			totalSum += i + 1;
+		}
+		if (totalSum % 2 == 1) {
+			out.println(0);
+			out.close();
+			in.close();
+			return;
+		}
+		System.out.println((bruteForce(ints, numInts - 1, totalSum / 2)) / 2);
 		out.close();
 		in.close();
+	}
+
+	static int bruteForce(int[] nums, int currNumInd, int currSum) {
+		if (currSum == 0) {
+			return 1;
+		}
+		if (currSum < 0) {
+			return 0;
+		}
+		if (currSum > 0 && currNumInd < 0) {
+			return 0;
+		}
+//		Sum of bruteForce including currNum and excluding currNum
+		return bruteForce(nums, currNumInd - 1, currSum) + bruteForce(nums, currNumInd - 1, currSum - nums[currNumInd]);
 	}
 }
