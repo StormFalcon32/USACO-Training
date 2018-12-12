6006cc6de89b88ed40cfc59f64e65c622a375d1b
StormFalcon32 <benchen9708@gmail.com>
2018-12-11 21:14:43 -0500

    Finished subset

--- Training/src/subset.java
+++ Training/src/subset.java
@@ -11,12 +11,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class subset {
 
 	static int numInts;
 	static int totalSum;
+	static HashMap<State, Long> states = new HashMap<State, Long>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("subset.in"));
@@ -30,16 +32,18 @@ public class subset {
 		}
 		if (totalSum % 2 == 1) {
 			out.println(0);
-			out.close();
-			in.close();
-			return;
+		} else {
+			out.println((bruteForce(ints, numInts - 1, totalSum / 2)) / 2);
 		}
-		System.out.println((bruteForce(ints, numInts - 1, totalSum / 2)) / 2);
 		out.close();
 		in.close();
 	}
 
-	static int bruteForce(int[] nums, int currNumInd, int currSum) {
+	static long bruteForce(int[] nums, int currNumInd, int currSum) {
+		State curr = new State(currNumInd, currSum);
+		if (states.containsKey(curr)) {
+			return states.get(curr);
+		}
 		if (currSum == 0) {
 			return 1;
 		}
@@ -49,7 +53,36 @@ public class subset {
 		if (currSum > 0 && currNumInd < 0) {
 			return 0;
 		}
-//		Sum of bruteForce including currNum and excluding currNum
-		return bruteForce(nums, currNumInd - 1, currSum) + bruteForce(nums, currNumInd - 1, currSum - nums[currNumInd]);
+		// Sum of bruteForce including currNum and excluding currNum
+		long result = bruteForce(nums, currNumInd - 1, currSum) + bruteForce(nums, currNumInd - 1, currSum - nums[currNumInd]);
+		states.put(curr, result);
+		return result;
+	}
+
+	static class State {
+		int numInd;
+		int sum;
+
+		public State(int a, int b) {
+			numInd = a;
+			sum = b;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			State other = (State) o;
+			if (this.numInd == other.numInd && this.sum == other.sum) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			int hash = 7;
+			hash = 31 * hash + numInd;
+			hash = 31 * sum;
+			return hash;
+		}
 	}
 }
