0ee3dd5348d58c3f21aa6973f0ac75bb81e1f53c
StormFalcon32 <benchen9708@gmail.com>
2019-01-22 21:49:27 -0500

    Almost done with comehome

--- Training/src/comehome.java
+++ Training/src/comehome.java
@@ -16,50 +16,73 @@ import java.util.StringTokenizer;
 
 public class comehome {
 	
-	static String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+	static String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("comehome.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("comehome.out")));
 		StringTokenizer ln = new StringTokenizer(in.readLine());
 		int PATHS = Integer.parseInt(ln.nextToken());
-		int[][] adjMat = new int[26][26];
+		int[][] adjMat = new int[52][52];
+		for (int i = 0; i < 52; i++) {
+			for (int j = 0; j < 52; j++) {
+				adjMat[i][j] = 1 << 20;
+			}
+		}
 		ArrayList<Integer> startingPoints = new ArrayList<Integer>();
 		for (int i = 0; i < PATHS; i++) {
 			ln = new StringTokenizer(in.readLine());
 			String f = ln.nextToken();
 			String l = ln.nextToken();
+			int fNum = 0;
+			int lNum = 0;
 			int dist = Integer.parseInt(ln.nextToken());
 			if (f.equals(f.toUpperCase()) && !f.equals("Z")) {
 				startingPoints.add(f.toUpperCase().charAt(0) - 'A');
+				fNum = f.toUpperCase().charAt(0) - 'A';
+			} else {
+				fNum = (f.toUpperCase().charAt(0) - 'A') + 26;
 			}
-			adjMat[f.toUpperCase().charAt(0) - 'A'][l.toUpperCase().charAt(0) - 'A'] = dist;
+			if (l.equals(l.toUpperCase()) && !l.equals("Z")) {
+				startingPoints.add(l.toUpperCase().charAt(0) - 'A');
+				lNum = l.toUpperCase().charAt(0) - 'A';
+			} else {
+				lNum = (l.toUpperCase().charAt(0) - 'A') + 26;
+			}
+			if (f.equals("Z")) {
+				fNum = 25;
+			}
+			if (l.equals("Z")) {
+				lNum = 25;
+			}
+			adjMat[fNum][lNum] = dist;
+			adjMat[lNum][fNum] = dist;
 		}
 		int[] distances = dijkstra(adjMat, 25);
 		String bestLetter = "";
 		int best = Integer.MAX_VALUE;
 		for (int i = 0; i < startingPoints.size(); i++) {
 			if (distances[startingPoints.get(i)] < best) {
-				best = adjMat[25][startingPoints.get(i)];
+				best = distances[startingPoints.get(i)];
 				bestLetter = alphabet[startingPoints.get(i)];
 			}
 		}
-		System.out.println(bestLetter + " " + best);
+		out.println(bestLetter + " " + best);
 		out.close();
 		in.close();
 	}
 	
 	static int[] dijkstra(int[][] adj, int root) {
-		int[] dist = new int[26];
+		int[] dist = new int[52];
 		Arrays.fill(dist, 1 << 20);
-		boolean[] inSet = new boolean[26];
+		boolean[] inSet = new boolean[52];
 		dist[root] = 0;
 		
-		for (int k = 0; k < 26 - 1; k++) {
+		for (int k = 0; k < 50; k++) {
 			
 			int u = -1;
 			int min = Integer.MAX_VALUE;
-			for (int i = 0; i < 26; i++) {
+			for (int i = 0; i < 52; i++) {
 				if (!inSet[i] && dist[i] < min) {
 					u = i;
 					min = dist[i];
@@ -67,7 +90,7 @@ public class comehome {
 			}
 			inSet[u] = true;
 			
-			for (int v = 0; v < 26; v++) {
+			for (int v = 0; v < 52; v++) {
 				int distThroughU = dist[u] + adj[u][v];
 				if (!inSet[v] && adj[u][v] != 0 && distThroughU < dist[v]) {
 					dist[v] = distThroughU;
