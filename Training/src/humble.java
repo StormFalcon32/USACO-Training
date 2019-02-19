67d4eb88a44908a0756c7625f5ce4a01bb59d649
StormFalcon32 <benchen9708@gmail.com>
2019-02-18 21:38:32 -0500

    Finished humble

--- Training/src/humble.java
+++ Training/src/humble.java
@@ -1,3 +1,4 @@
+
 /*
 ID: benchen1
 LANG: JAVA
@@ -10,6 +11,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class humble {
@@ -25,8 +27,30 @@ public class humble {
 		for (int i = 0; i < K; i++) {
 			nums[i] = Integer.parseInt(ln.nextToken());
 		}
-		int[] humble = new int[N + 1];
-		System.out.println(humble[N]);
+		long[] humble = new long[N + 1];
+		int[] prev = new int[K + 1];
+		humble[0] = 1;
+		for (int i = 0; i < N; i++) {
+			long next = Long.MAX_VALUE;
+			ArrayList<Integer> possible = new ArrayList<Integer>();
+			for (int j = 0; j < K; j++) {
+				long check = nums[j] * humble[prev[j]];
+				if (check > humble[i] && check <= next) {
+					if (check < next) {
+						next = check;
+						possible.clear();
+						possible.add(j);
+					} else if (check == next) {
+						possible.add(j);
+					}
+				}
+			}
+			humble[i + 1] = next;
+			for (int j : possible) {
+				prev[j]++;
+			}
+		}
+		out.println(humble[N]);
 		out.close();
 		in.close();
 	}
